dbdf9bec4329d46e4c7699339da5dc09be2576fa
Henner W <henner@tapwork.de>
2015-06-26 00:48:33 +0200

    fixed http headers so that download works correct in safari

--- download.php
+++ download.php
@@ -23,10 +23,17 @@ if (authorized()) {
     $file = $downloadPath . $file;
     if (file_exists($file))
     {
+      header("Pragma: public");
+      header("Expires: 0");
+      header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+      header("Cache-Control: public");
+      header("Content-Description: File Transfer");
+      header("Content-type: application/octet-stream");
+      header("Content-Disposition: attachment; filename=\"".basename($file)."\"");
+      header("Content-Transfer-Encoding: binary");
       header("Content-Length: " . filesize($file));
-      header('Content-Disposition: attachment; filename="'.basename($file).'"');
-      // Send file for download
-       readfile($file);
+      ob_end_flush();
+      @readfile($file);
     }
     else
     {
